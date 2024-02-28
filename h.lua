d8a3d1e84e019b3aa77c143727ae9a3ad79cd177
I-EatYourBeets <121991132+I-EatYourBeets@users.noreply.github.com>
2024-02-27 20:27:27 -0800

    Add files via upload
    h

---
+++ h.lua
@@ -0,0 +1,7 @@
+for i = 1, 10000 do
+    if math.floor(i/2) == i/2 then
+        print("h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h ")
+    else
+        print(" h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h h")
+    end
+end
